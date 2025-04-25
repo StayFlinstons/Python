a3eca676152c109874262a4e0e6d90e4f00838de
Richard <88545769+StayFlinstons@users.noreply.github.com>
2025-04-25 17:53:32 -0300

    resken

---
+++ bd_python/script.py
@@ -0,0 +1,58 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, String, Integer
+
+# configurando a engine do BD
+engine = create_engine("sqlite:///database.db")
+
+# configurando a sessão
+Session = sessionmaker(engine)
+
+# criando tablea
+Base = declarative_base()
+
+class Usuario(Base):
+    __tablename__ = 'usuarios'
+    id = Column(Integer, primary_key=True)
+    nome = Column(String, nullable=False)
+    tipo = Column(String, nullable=False)
+
+def insert_usuario(nome_usuario, tipo_usuario):
+    session = Session()
+    try:
+        if all([nome_usuario, tipo_usuario]):
+            usuario = Usuario(nome=nome_usuario, tipo=tipo_usuario)
+            session.add(usuario)
+            session.commit()
+            print(f'Usuário {nome_usuario} cadastrado com sucesso!')
+        else:
+            print('É obrigatorio preencher o nome e tipo do usuário!')
+    except Exception as e:
+        session.rollback()
+        print(f'Ocorreu um erro ao tentar cadastrar o usuário {nome_usuario}: {e}')
+    finally:
+        session.close()
+
+def select_usuarios(nome_usuario=''):
+    session = Session()
+    try:
+        if nome_usuario:
+            dados = session.query(Usuario).filter(Usuario.nome == nome_usuario)
+        else:
+            dados = session.query(Usuario).all()
+
+        for i in dados:
+            print(f'Usuário: {i.nome} - Tipo: {i.tipo}')
+
+    except Exception as e:
+        print('Ocorreu algum erro ao consultar o(s) usuário(s)!')
+    finally:
+        session.close()
+
+if __name__ == '__main__':
+    os.system('cls')
+    Base.metadata.create_all(engine)
+    # insert_usuario('Toli', 'Bola')
+    select_usuarios('Richard')
